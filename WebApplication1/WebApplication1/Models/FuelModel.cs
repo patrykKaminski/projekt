b3a47766cf7b1bbcc383e13db8d9cf9003005788
Patryk Kamiński <patkamin92@gmail.com>
2017-11-18 09:47:49 +0100

    Dodanie Modeli

---
+++ WebApplication1/WebApplication1/Models/FuelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class FuelModel
+    {
+        public int Fuel { get; set; }
+        public int Distance { get; set; }
+        public double AvgConsumption { get; set; }
+    }
+}
