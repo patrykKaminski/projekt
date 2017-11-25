ec81d629780916c5044fc3b881d04eda3369e48a
Patryk Kamiński <patkamin92@gmail.com>
2017-11-25 11:41:18 +0100

    g

---
+++ Auto/Auto/Models/AutoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Auto.Models
+{
+    public class AutoModel : DbContext
+    {
+        [Required]
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string NrRejestracyjny { get; set; }
+    }
+}
